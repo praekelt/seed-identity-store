1241208505c062741500fa03fcf52796ef2eca40
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-03-14 13:09:04 +0200

    removed mysterious The_Incr task

--- identities/tasks.py
+++ identities/tasks.py
@@ -1,29 +1,7 @@
 import json
 import requests
 from celery.task import Task
-from celery.utils.log import get_task_logger
 from django.conf import settings
-
-
-logger = get_task_logger(__name__)
-
-
-class The_Incr(Task):
-
-    """
-    Task to incr something
-    """
-    name = "seed_identity_store.identities.tasks.the_incr"
-
-    def run(self, anum, **kwargs):
-        """
-        Returns an incr'd number
-        """
-        l = self.get_logger(**kwargs)
-        l.info("Incrementing <%s>" % (anum,))
-        return int(anum)+1
-
-the_incr = The_Incr()
 
 
 class DeliverHook(Task):
