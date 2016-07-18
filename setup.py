85ef9fe128c163b3de23429d1f41a9d16f6e30da
Mike Jones <mike@westerncapelabs.com>
2016-07-18 22:29:08 +0200

    Bump packages for better hook support

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'Django==1.9.1',
         'djangorestframework==3.3.2',
         'dj-database-url==0.3.0',
-        'psycopg2==2.6.1',
+        'psycopg2==2.6.2',
         'raven==5.10.0',
         'gunicorn==19.4.5',
         'django-filter==0.12.0',
@@ -22,7 +22,7 @@ setup(
         'django-celery==3.1.17',
         'redis==2.10.5',
         'pytz==2015.7',
-        'django-rest-hooks==1.2.1',
+        'django-rest-hooks==1.3.1',
         'go-http==0.3.0'
     ],
     classifiers=[
