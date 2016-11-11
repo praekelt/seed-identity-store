6e2a775b66cec6a53184cc4dffdfa65774ceaa7e
Simon de Haan <simon@praekelt.org>
2016-11-11 20:31:33 +0100

    fix sentry logging for celery

--- seed_identity_store/celery.py
+++ seed_identity_store/celery.py
@@ -2,12 +2,25 @@ from __future__ import absolute_import
 
 import os
 
-from celery import Celery
+import celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
 
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seed_identity_store.settings')
+
+
+class Celery(celery.Celery):
+
+    def on_configure(self):
+        client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+        # hook into the Celery error handler
+        register_signal(client)
+
 
 app = Celery('seed_identity_store')
 
